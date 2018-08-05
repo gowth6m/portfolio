9c3b047ddfceb64a1c2b2c2c6678d8c11d9cf37a
Gowthaman <gowt1729@gmail.com>
2018-08-05 22:09:56 +0100

    5 Aug 2k18 (10:09pm)

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.menu-toggle').click(function() {
+        window.alert("lol");
+        $('nav-bar0').toggleClass('nav-bar0-open', 500);
+        
+    })
+})
